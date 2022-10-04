1e3aa602bbca8b257ff538bc41b83fc81ccf7832
Ian Ctn <55019@etu.he2b.be>
2022-10-04 17:32:52 +0200

    data binding

--- Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Jackie The Frite")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+
+
     }
 }
