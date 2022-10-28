dc595ee5c47f6a82c6dde25a83ff4cd2475839ce
Ian Ctn <55019@etu.he2b.be>
2022-10-28 11:38:53 +0200

    fun fun fun fun

--- GuessTheWord-Starter/app/build/generated/source/kapt/debug/com/example/android/guesstheword/databinding/ScoreFragmentBindingImpl.java
+++ GuessTheWord-Starter/app/build/generated/source/kapt/debug/com/example/android/guesstheword/databinding/ScoreFragmentBindingImpl.java
@@ -14,8 +14,7 @@ public class ScoreFragmentBindingImpl extends ScoreFragmentBinding  {
     static {
         sIncludes = null;
         sViewsWithIds = new android.util.SparseIntArray();
-        sViewsWithIds.put(R.id.you_scored_text, 1);
-        sViewsWithIds.put(R.id.score_text, 2);
+        sViewsWithIds.put(R.id.you_scored_text, 2);
         sViewsWithIds.put(R.id.play_again_button, 3);
     }
     // views
@@ -28,13 +27,14 @@ public class ScoreFragmentBindingImpl extends ScoreFragmentBinding  {
         this(bindingComponent, root, mapBindings(bindingComponent, root, 4, sIncludes, sViewsWithIds));
     }
     private ScoreFragmentBindingImpl(androidx.databinding.DataBindingComponent bindingComponent, View root, Object[] bindings) {
-        super(bindingComponent, root, 0
+        super(bindingComponent, root, 1
             , (android.widget.Button) bindings[3]
             , (androidx.constraintlayout.widget.ConstraintLayout) bindings[0]
-            , (android.widget.TextView) bindings[2]
             , (android.widget.TextView) bindings[1]
+            , (android.widget.TextView) bindings[2]
             );
         this.scoreLayout.setTag(null);
+        this.scoreText.setTag(null);
         setRootTag(root);
         // listeners
         invalidateAll();
@@ -43,7 +43,7 @@ public class ScoreFragmentBindingImpl extends ScoreFragmentBinding  {
     @Override
     public void invalidateAll() {
         synchronized(this) {
-                mDirtyFlags = 0x1L;
+                mDirtyFlags = 0x4L;
         }
         requestRebind();
     }
@@ -61,12 +61,38 @@ public class ScoreFragmentBindingImpl extends ScoreFragmentBinding  {
     @Override
     public boolean setVariable(int variableId, @Nullable Object variable)  {
         boolean variableSet = true;
+        if (BR.scoreViewModel == variableId) {
+            setScoreViewModel((com.example.android.guesstheword.screens.score.ScoreViewModel) variable);
+        }
+        else {
+            variableSet = false;
+        }
             return variableSet;
+    }
+
+    public void setScoreViewModel(@Nullable com.example.android.guesstheword.screens.score.ScoreViewModel ScoreViewModel) {
+        this.mScoreViewModel = ScoreViewModel;
+        synchronized(this) {
+            mDirtyFlags |= 0x2L;
+        }
+        notifyPropertyChanged(BR.scoreViewModel);
+        super.requestRebind();
     }
 
     @Override
     protected boolean onFieldChange(int localFieldId, Object object, int fieldId) {
         switch (localFieldId) {
+            case 0 :
+                return onChangeScoreViewModelScore((androidx.lifecycle.LiveData<java.lang.Integer>) object, fieldId);
+        }
+        return false;
+    }
+    private boolean onChangeScoreViewModelScore(androidx.lifecycle.LiveData<java.lang.Integer> ScoreViewModelScore, int fieldId) {
+        if (fieldId == BR._all) {
+            synchronized(this) {
+                    mDirtyFlags |= 0x1L;
+            }
+            return true;
         }
         return false;
     }
@@ -78,14 +104,51 @@ public class ScoreFragmentBindingImpl extends ScoreFragmentBinding  {
             dirtyFlags = mDirtyFlags;
             mDirtyFlags = 0;
         }
+        java.lang.Integer scoreViewModelScoreGetValue = null;
+        int androidxDatabindingViewDataBindingSafeUnboxScoreViewModelScoreGetValue = 0;
+        java.lang.String stringValueOfScoreViewModelScore = null;
+        com.example.android.guesstheword.screens.score.ScoreViewModel scoreViewModel = mScoreViewModel;
+        androidx.lifecycle.LiveData<java.lang.Integer> scoreViewModelScore = null;
+
+        if ((dirtyFlags & 0x7L) != 0) {
+
+
+
+                if (scoreViewModel != null) {
+                    // read scoreViewModel.score
+                    scoreViewModelScore = scoreViewModel.getScore();
+                }
+                updateLiveDataRegistration(0, scoreViewModelScore);
+
+
+                if (scoreViewModelScore != null) {
+                    // read scoreViewModel.score.getValue()
+                    scoreViewModelScoreGetValue = scoreViewModelScore.getValue();
+                }
+
+
+                // read androidx.databinding.ViewDataBinding.safeUnbox(scoreViewModel.score.getValue())
+                androidxDatabindingViewDataBindingSafeUnboxScoreViewModelScoreGetValue = androidx.databinding.ViewDataBinding.safeUnbox(scoreViewModelScoreGetValue);
+
+
+                // read String.valueOf(androidx.databinding.ViewDataBinding.safeUnbox(scoreViewModel.score.getValue()))
+                stringValueOfScoreViewModelScore = java.lang.String.valueOf(androidxDatabindingViewDataBindingSafeUnboxScoreViewModelScoreGetValue);
+        }
         // batch finished
+        if ((dirtyFlags & 0x7L) != 0) {
+            // api target 1
+
+            androidx.databinding.adapters.TextViewBindingAdapter.setText(this.scoreText, stringValueOfScoreViewModelScore);
+        }
     }
     // Listener Stub Implementations
     // callback impls
     // dirty flag
     private  long mDirtyFlags = 0xffffffffffffffffL;
     /* flag mapping
-        flag 0 (0x1L): null
+        flag 0 (0x1L): scoreViewModel.score
+        flag 1 (0x2L): scoreViewModel
+        flag 2 (0x3L): null
     flag mapping end*/
     //end
 }
--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,11 +1,35 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+    companion object{
+        private const val DONE = 0L
+
+        private const val ONE_SECOND = 1000L
+
+        private const val COUNTDOWN_TIME = 60000L
+
+    }
+
+    private val timer: CountDownTimer
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+
+
     // The current word
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
@@ -30,11 +54,26 @@ class GameViewModel : ViewModel() {
         _score.value = 0
         resetList()
         nextWord()
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = (millisUntilFinished / ONE_SECOND)
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
+
+        timer.start()
     }
 
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed!")
+        // Cancel the timer
+        timer.cancel()
+
     }
 
     /**
@@ -72,11 +111,12 @@ class GameViewModel : ViewModel() {
      */
     private fun nextWord() {
         if (wordList.isEmpty()) {
-            onGameFinish()
+            resetList()
         } else {
-            //Select and remove a _word from the list
+            // Remove a word from the list
             _word.value = wordList.removeAt(0)
         }
+
 
     }
 
